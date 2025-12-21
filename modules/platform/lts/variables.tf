47505ba7a6c2ef12224a2514824733e65b842bbc
Matheus Farias <matheusfarias519@gmail.com>
2025-12-21 18:45:16 -0300

    Updated LTS variables.

--- modules/platform/lts/variables.tf
+++ modules/platform/lts/variables.tf
@@ -1,3 +1,13 @@
+variable "lts_group_name" {
+  type = string
+  default = "cci_log_group"
+}
+
+variable "ttl_in_days" {
+  type = number
+  default = 7
+}
+
 variable "awesome_stream_name" {
   type = string
   default = "awesome_stream"
