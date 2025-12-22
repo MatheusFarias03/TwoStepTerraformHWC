737eef669bd8b2e6876f407b13d07f33848822d2
Matheus Farias de Oliveira Matsumoto (A) <m00929874@china.huawei.com>
2025-12-22 13:36:10 -0300

    Added domain in WAF

--- modules/platform/waf/main.tf
+++ modules/platform/waf/main.tf
@@ -12,3 +12,18 @@ resource "huaweicloud_waf_cloud_instance" "waf_instance" {
   website = "hec-hk"
   enterprise_project_id = var.enterprise_project_id
 }
+
+resource "huaweicloud_waf_domain" "name" {
+  domain = var.domain_name
+  enterprise_project_id = var.enterprise_project_id
+  website_name = var.website_name
+  protect_status = 1 # 1 means enabled.
+
+  server {
+    client_protocol = "HTTPS"
+    server_protocol = "HTTP"
+    address = var.domain_name # Should be the IP or domain name of the server.
+    port = 80
+    type = "ipv4"
+  }
+}
--- modules/platform/waf/variables.tf
+++ modules/platform/waf/variables.tf
@@ -2,3 +2,13 @@
 variable "enterprise_project_id" {
   type = string
 }
+
+# Local variables.
+variable "website_name" {
+  type = string
+  default = "Awesome Inc."
+}
+variable "domain_name" {
+  type = string
+  default = "awesome.com.br"
+}
